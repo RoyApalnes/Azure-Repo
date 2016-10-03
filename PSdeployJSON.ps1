513887e2012d91da384e75f2e99d242021b893c5
Roy Apalnes <roy.apalnes@gmail.com>
2016-10-03 08:56:23 +0200

    Added initial documents

---
+++ PSdeployJSON.ps1
@@ -0,0 +1,10 @@
+﻿#Datacenter location
+$locName = "northeurope"
+#Resource Group Name
+$rgName = "Demo"
+
+New-AzureRmResourceGroup -Name $rgName -Location $locName
+
+$deployName="DEMO-DC01"
+$templatePath = "C:\Users\Roy\OneDrive\Azure\ADDS\VirtualMachineTemplate1.json"
+$parameterFile = "C:\Users\Roy\OneDrive\Azure\ADDS\Parameters1.json"
