f0580979f74f170040247376f05ca0e98b89abc0
Roy Apalnes <roy.apalnes@gmail.com>
2016-10-10 15:23:36 +0200

    New stuff

---
+++ SetDNSvNet.ps1
@@ -0,0 +1,11 @@
+﻿#Modify as necessary. Maximum 12 DNS servers per vNet.
+$DNSIPs = "10.0.0.4"
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $rgName -name $vNetName
+ 
+$vnet.DhcpOptions.DnsServers = $null
+foreach ($IP in $DNSIPs)
+{
+$vnet.DhcpOptions.DnsServers += $IP
+}
+ 
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
