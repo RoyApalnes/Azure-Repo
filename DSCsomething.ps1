83429f71b26899609591ba41e04c97a1358c5062
Roy Apalnes <roy.apalnes@gmail.com>
2016-10-19 10:00:10 +0200

    New stuff

---
+++ DSCsomething.ps1
@@ -0,0 +1,13 @@
+﻿Install-Module xDscResourceDesigner
+Import-Module xDscResourceDesigner
+
+
+$UserName = New-xDscResourceProperty –Name UserName -Type String -Attribute Key
+$Ensure = New-xDscResourceProperty –Name Ensure -Type String -Attribute Write –ValidateSet “Present”, “Absent”
+$DomainCredential = New-xDscResourceProperty –Name DomainCredential-Type PSCredential -Attribute Write
+$Password = New-xDscResourceProperty –Name Password -Type PSCredential -Attribute Write
+
+New-xDscResource –Name Demo_ADUser –Property $UserName, $Ensure, $DomainCredential, $Password –Path ‘C:\Program Files\WindowsPowerShell\Modules’ –ModuleName Demo_DSCModule
+
+$lastLogon = New-xDscResourceProperty –Name LastLogon –Type Hashtable –Attribute Write –Description “For mapping users to their last log on time”
+Update-xDscResource –Name ‘Demo_ADUser’ –Property $UserName, $Ensure, $DomainCredential, $Password, $lastLogon -Force
---
+++ GetPublisherOffersSkuVersion.ps1
@@ -0,0 +1,17 @@
+﻿#DataCenter Location
+$loc = "North Europe"
+#Find all the available publishers
+Get-AzureRmVMImagePublisher -Location $loc | Select PublisherName
+#Pick a publisher
+$pubName="MicrosoftWindowsServer"
+Get-AzureRmVMImageOffer -Location $loc -Publisher $pubName | Select Offer
+#Pick a specific offer
+$offerName="WindowsServer"
+#View the different SKUs
+Get-AzureRmVMImageSku -Location $loc -Publisher $pubName -Offer $offerName | Select Skus
+#Pick a specific SKU
+$skuName = "2016-Nano-Server"
+#View the versions of a SKU
+Get-AzureRmVMImage -Location $loc -PublisherName $pubName -Offer $offerName -Skus $skuName
+#Pick a specific image version
+$version = "2016.0.20161010"
